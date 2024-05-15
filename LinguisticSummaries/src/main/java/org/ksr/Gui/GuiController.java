094de35e63a39c725a7e3e79edf9029e8bd23452
Kacper Czernik <Kacper-czernik@wp.pl>
2024-05-15 23:21:05 +0200

    i do not wont to see guis anymore

--- LinguisticSummaries/src/main/java/org/ksr/Gui/GuiController.java
+++ LinguisticSummaries/src/main/java/org/ksr/Gui/GuiController.java
@@ -8,31 +8,14 @@ public class GuiController {
 
     @FXML
     private Button buttonGenerateLS;
-    @FXML
-    private Accordion menuAccordion;
 
     @FXML
     public void initialize() {
-        addPane("xd");
     }
 
     @FXML
     public void handleGenerateLSButtonClick() {
         System.out.println("Generate linguistic summary button clicked!");
-        addPane("xd");
     }
-
-    @FXML
-    public void addPane(String name){
-        TitledPane tPane = new TitledPane();
-        CheckBox checkBox = new CheckBox("apply");
-        VBox content = new VBox(checkBox);
-        tPane.setText(name);
-        tPane.setContent(content);
-
-        this.menuAccordion.getPanes().add(tPane);
-    }
-
-
 
 }
