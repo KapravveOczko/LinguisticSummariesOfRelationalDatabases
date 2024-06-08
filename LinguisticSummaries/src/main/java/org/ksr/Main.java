4abc62dd91b0b15805f2abdcffe05ee65b441d5e
Kacper Czernik <Kacper-czernik@wp.pl>
2024-06-08 19:40:55 +0200

    LinguisticSummaries | minor mistakes repaired

--- LinguisticSummaries/src/main/java/org/ksr/Main.java
+++ LinguisticSummaries/src/main/java/org/ksr/Main.java
@@ -15,32 +15,5 @@ public class Main {
         Gui gui = new Gui();
         gui.launchGui();
 
-        /*
-        DatabaseConnector db = new DatabaseConnector("jdbc:postgresql://localhost:5432/ksr", "postgres", "");
-        Assets assets = new Assets();
-
-        Label quantifier = new Label("about one third", assets.getVariable("relative_quantifiers"));
-        Label qualifier = new Label("east of Ireland", assets.getVariable("longitude"));
-
-        List<LinguisticVariable> summarizers = new ArrayList<>();
-        summarizers.add(assets.getVariable("sea_surface_temperature"));
-        summarizers.add(assets.getVariable("significant_wave_height"));
-        summarizers.add(assets.getVariable("mixed_layer_depth"));
-//        summarizers.add(assets.getVariable("mean_wave_direction"));
-
-
-        //--------------------------------------------------------------------------------------------------------------//
-//        LinguisticSummary testSummary = new LinguisticSummary(db, qualifier, summarizers, quantifier);
-//        List<String> summaries = testSummary.createLinguisticSummary();
-        //-------------------------------------------------------------------------------------------------------------//
-//        LinguisticSummaryTwoSubject testSummaryTwoSubject = new LinguisticSummaryTwoSubject(db, "test_south_data", "test_north_data", qualifier, summarizers, quantifier);
-//        List<String> summaries = testSummaryTwoSubject.createLinguisticSummaryTwoSubject();
-
-
-//        for(String summary : summaries){
-//            System.out.println(summary);
-//        }
-         */
-
     }
 }
